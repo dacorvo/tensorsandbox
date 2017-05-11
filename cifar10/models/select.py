e9ef2e5facc943aedd29fab54ba0a65f61f73e10
David Corvoysier <david.corvoysier@intel.com>
2017-05-11 15:51:28 +0200

    cifar10: implement Tensorflow tutorial version
    Using the same network topology as in the tutorial code, but a
    simplified training environment, we reach 75% accuracy in 10000
    iterations, without data augmentation nor varables moving average.
    
    With data augmentation and moving averages, the tutorial achieves
    81%/10000 and 86%/100000 respectively.

--- cifar10/models/select.py
+++ cifar10/models/select.py
@@ -1,13 +1,16 @@
 import tensorflow as tf
 
 import cs231n
+import tuto
 
-tf.app.flags.DEFINE_string('model', 'cnn',
-                               """One of [cs231n, cnn, tuto].""")
+tf.app.flags.DEFINE_string('model', 'cs231n',
+                               """One of [cs231n, tuto].""")
 
 def by_name(name):
     if name == 'cs231n':
         model = cs231n.Cs231n()
+    elif name == 'tuto':
+        model = tuto.Tuto()
     else:
         raise ValueError('No such model %s' % name)
     return model
