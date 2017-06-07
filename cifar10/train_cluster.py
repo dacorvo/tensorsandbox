2df72310e1b24b672b37f58886e2edbc53728181
David Corvoysier <david.corvoysier@gmail.com>
2017-06-07 12:18:36 +0200

    cifar/cluster: remember cluster configuration between runs

--- cifar10/train_cluster.py
+++ cifar10/train_cluster.py
@@ -15,27 +15,52 @@ import tensorflow as tf
 import time
 from datetime import datetime
 import os
+import json
 
 import train
 import models.data as data
 
 FLAGS = tf.app.flags.FLAGS
 
+# Setup flags
 tf.app.flags.DEFINE_string('ps_hosts', '',
                            """Comma-separated list of ps hosts""")
 tf.app.flags.DEFINE_string('worker_hosts', '',
                            """Comma-separated list of worker hosts""")
-tf.app.flags.DEFINE_string('job_name', 'ps',
+# Run flags
+tf.app.flags.DEFINE_string('job_name', '',
                            """Job name: either ps or worker.""")
 tf.app.flags.DEFINE_integer('task_index', 0,
                             """Task index in the current job.""")
 
 def main(argv=None):
     data.maybe_download_and_extract(FLAGS.data_dir)
-    # Create a cluster from the parameter server and worker hosts.
-    ps_hosts = FLAGS.ps_hosts.split(",")
-    worker_hosts = FLAGS.worker_hosts.split(",")
-    cluster = tf.train.ClusterSpec({"ps": ps_hosts, "worker": worker_hosts})
+    # If cluster configuration flags were provided, save them
+    if FLAGS.ps_hosts != '' and FLAGS.worker_hosts != '':
+        ps_hosts = FLAGS.ps_hosts.split(",")
+        worker_hosts = FLAGS.worker_hosts.split(",")
+        cluster_config = {"ps": ps_hosts, "worker": worker_hosts}
+        # Save cluster configuration
+        with open('cluster.json', 'w') as f:
+            json.dump(cluster_config, f)
+        print('Cluster configuration saved.')
+    else:
+        try:
+            # Read cluster configuration
+            with open('cluster.json', 'r') as f:
+                cluster_config = json.load(f)
+        except (OSError, IOError) as e:
+            print("No cluster configuration found: you need to provide at " \
+                  "least once the two lists of ps and worker hosts")
+            return
+
+    if FLAGS.job_name =='':
+        print('Pass this script a job name (ps or worker) to start a ' \
+              'training session.')
+        return
+
+    # Create a cluster
+    cluster = tf.train.ClusterSpec(cluster_config)
 
     # Create and start a server for the local task.
     server = tf.train.Server(cluster,
